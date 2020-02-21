032b83259d3ba21e0557a6098e5284b2578b4e46
andynarf <untalandy@gmail.com>
2020-02-21 14:53:28 -0300

    added readme

--- webpack.config.js
+++ webpack.config.js
@@ -4,25 +4,24 @@ const webpack = require('webpack');
 module.exports = {
   entry: './src/index.js',
   output: {
-     filename: 'main.js',
-     filename: 'bundle.js',
+    filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
-     module: {
-         rules: [
-           {
-             test: /\.css$/,
-             use: [
-               'style-loader',
-               'css-loader',
-             ],
-           },
-           {
-               test: /\.(png|svg|jpg|gif)$/,
-               use: [
-                   'file-loader',
-               ],
-           },
-         ],
-       },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+        ],
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          'file-loader',
+        ],
+      },
+    ],
+  },
 };
